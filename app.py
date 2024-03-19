f8ecb76490259edce04476f713b69d4499c475cc
nikohadiqi <nikohadiqi@gmail.com>
2024-03-19 23:04:01 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+def luas_segitiga():
+    a = int(input("Masukkan Alas Segitiga :  "))
+    t = int(input("Masukkan Tinggi Segitiga :  "))
+    luas = a * t / 2
+    print("Luas Segitiga : ", luas)
+
+luas_segitiga()
+
